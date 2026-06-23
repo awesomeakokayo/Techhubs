8fac142ed0e35bfa2842d0daca486b2e7306b1c5
awesomeakokayo <awesomeakokayo@gmail.com>
2026-06-23 01:18:22 -0700

    fix: force-dynamic on account page to prevent build crash

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,6 +3,8 @@ import { prisma } from '@/lib/prisma'
 import { redirect } from 'next/navigation'
 import { AccountClient } from './AccountClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AccountPage() {
   const session = await auth()
   if (!session?.user?.id) redirect('/login')
